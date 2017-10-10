d98e95dfaeb61b0db113d70bcb56e7c4163ceaea
sdhoju <sdhoju@go.olemiss.edu>
2017-10-10 17:42:07 -0500

    ready

---
+++ map.js
@@ -0,0 +1,38 @@
+		
+
+d3.csv("usss.csv", function(error, data) {
+	
+ var groupData= d3.nest()
+   				 .key(function(d){return d['employer_state']; })
+				 .rollup("len")
+   				 .rollup(function(v){return v.length; })
+   				 .entries(data);
+
+function tooltipHtml(n, d){	/* function to create html content string in tooltip div. */
+		return "<h4>"+n+"</h4><table>"+
+			"<tr><td>No of cases</td><td>"+(d.no_of_cases)+"</td></tr>"+
+			"</table>";
+	}
+	var no_of_cases
+	count=0
+	var sampleData ={};	/* Sample random data. */	
+	["HI", "AK", "FL", "SC", "GA", "AL", "NC", "TN", "RI", "CT", "MA",
+	"ME", "NH", "VT", "NY", "NJ", "PA", "DE", "MD", "WV", "KY", "OH", 
+	"MI", "WY", "MT", "ID", "WA", "DC", "TX", "CA", "AZ", "NV", "UT", 
+	"CO", "NM", "OR", "ND", "SD", "NE", "IA", "MS", "IN", "IL", "MN", 
+	"WI", "MO", "AR", "OK", "KS", "LS", "VA"].forEach(function(d){ 
+			no_of_cases=groupData[count]['values'], 
+			sampleData[d]={no_of_cases, color:d3.interpolate("#ffffff", "#34000f")(no_of_cases/1000)}; 
+			//
+			count=count+1;
+		});
+	console.log(d3.max(groupData, function(d) {
+	 return +d['values'] }));
+	/* draw states on id #statesvg */	
+	uStates.draw("#statesvg", sampleData, tooltipHtml);
+	d3.select(self.frameElement).style("height", "800px"); 
+
+
+
+
+});
---
+++ visa.js
@@ -0,0 +1,65 @@
+var margin = {top: 20, right: 20, bottom: 120, left: 60},
+    width = 2400 - margin.left - margin.right,
+    height = 700 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+
+var y = d3.scale.linear().range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+   
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+  
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
+
+d3.csv("usss.csv", function(error, data) {
+	
+  var groupData= d3.nest()
+   				 .key(function(d){return d['country_of_citzenship']; })
+				 .rollup("len")
+   				 .rollup(function(v){return v.length; })
+   				 .entries(data);
+	console.log(groupData);
+  x.domain(groupData.map(function(d) { return d.key; }));
+  y.domain([0, d3.max(groupData, function(d) { return +d.values; })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis)
+    .selectAll("text")
+      .style("text-anchor", "end")
+      .attr("dx", "-.8em")
+      .attr("dy", "-.55em")
+      .attr("transform", "rotate(-90)" );
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+   
+
+  svg.selectAll("bar")
+      .data(groupData)
+    .enter().append("rect")
+      .style("fill", "steelblue")
+      .attr("x", function(d) { return x(d.key); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(+d.values); })
+      .attr("height", function(d) { return height - y(+d.values); });
+
+});
