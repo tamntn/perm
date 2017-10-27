41d562dbacc693b8edd07d6c564bde92705ece6e
Aishwant Ghimire <aghimire@go.olemiss.edu>
2017-10-27 12:37:09 -0500

    Model works when clicked on map

--- US_map/zoomMap.js
+++ US_map/zoomMap.js
@@ -27,6 +27,7 @@ var projection = d3.geo.albersUsa()
     .scale(1070)
     .translate([width / 2, height / 2]);
 
+var domain=[0,2,3,4,5];
 
 var path = d3.geo.path()
     .projection(projection);
@@ -68,32 +69,61 @@ d3.json("https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba16920754
       .attr("d", path);
 });
 
-function clicked(d) {
-  var x, y, k;
 
-  if (d && centered !== d) {
-    var centroid = path.centroid(d);
-    console.log(centroid)
-    x = centroid[0];
+	function clicked(d){
 
-    y = centroid[1];
+		d3.select('.modal-content')
+		.selectAll('p')
+		.data(domain)
+		.enter()
+		.append('p')
+		.text('Thank you for your Time')
+
+
+		modal.style.display = "block";
+		
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+		    modal.style.display = "none";
+		}
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+		    if (event.target == modal) {
+		        modal.style.display = "none";
+		    }
+		}
+	}
+
+
+// function clicked(d) {
+//   var x, y, k;
+
+//   if (d && centered !== d) {
+//     var centroid = path.centroid(d);
+//     console.log(centroid)
+//     x = centroid[0];
+
+//     y = centroid[1];
     
-    k = 10;
-    centered = d;
-  } else {
-    x = width/2 ;
-    y = height/2 ;
-    k = 1;
-    centered = null;
-  }
+//     k = 10;
+//     centered = d;
+//   } else {
+//     x = width/2 ;
+//     y = height/2 ;
+//     k = 1;
+//     centered = null;
+//   }
 
-  g.selectAll("path")
-      .classed("active", centered && function(d) { return d === centered; });
+//   g.selectAll("path")
+//       .classed("active", centered && function(d) { return d === centered; });
 
-  g.transition()
-      .duration(750)
-      .attr("transform", "translate(" + width /2 + "," + height /2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
-      .style("stroke-width", 1.5 / k + "px");
-}
+//   g.transition()
+//       .duration(750)
+//       .attr("transform", "translate(" + width /2 + "," + height /2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+//       .style("stroke-width", 1.5 / k + "px");
+// }
+
+
 });
 
