e86b01f7c0109c81bfd09eb119c50b31d460f6cc
Tam Nguyen <tam.nguyen9779@gmail.com>
2017-11-07 17:16:07 -0600

    Update Job Tree

--- Dataset/treeProcessing.py
+++ Dataset/treeProcessing.py
@@ -17,9 +17,38 @@ with open("createdFiles/acceptanceRateByJob.csv", 'rb') as csvFile:
         value = int(row[1])
         data['children'].append({
             "name": name,
-            "value": value
+            "value": value,
+            "children": []
         })
     csvFile.close()
+
+
+def updateDataTree(jobGroup, jobTitle):
+    for group in data['children']:
+        if group['name'] == jobGroup:
+            updated = False
+            for job in group['children']:
+                if job["name"] == jobTitle:
+                    job["value"] = job["value"] + 1
+                    updated = True
+                    break
+            if updated == False:
+                group['children'].append({
+                    "name": jobTitle,
+                    "value": 0
+                })
+            break
+
+
+with open("metaFiles/final.csv", 'rb') as csvMain:
+    reader = csv.reader(csvMain)
+    header = reader.next()
+    rows = [row for row in reader if row]
+    for row in rows:
+        jobGroup = row[14]
+        jobTitle = row[15]
+        updateDataTree(jobGroup, jobTitle)
+    csvMain.close()
 
 with open(treeFile, 'wb') as tree:
     json.dump(data, tree)
